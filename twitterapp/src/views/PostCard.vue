665fba02b53d8430cc6cefc1b80150243e37c9e5
coutinho-maycon <coutinho.maycon@outlook.com>
2021-12-13 15:44:25 -0300

    Inserido o cabeçalho e o cartão primeira página

---
+++ twitterapp/src/views/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="flex bg-gray-800 pt-4">
+      <div class="min-w-max mr-3">
+        <img class="h-20 rounded-full" src="https://i.pinimg.com/originals/5a/72/e1/5a72e1f05f9e2e1b76a8438a7490dc3b.jpg" alt="">
+      </div>
+      <div class="text-white">
+        <p class="font-bold">Maycon Coutinho<span class="font-thin">@coutinhoMaycon . 4h</span></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at tempus felis. Aenean ut mollis massa. Nam lacinia nisi eu dolor vestibulum iaculis. Quisque lobortis dignissim porta. In interdum velit ut nunc egestas posuere. Donec dictum elit sed arcu efficitur, vel venenatis dolor pretium. Quisque bibendum mauris nisi, non malesuada lorem hendrerit ut. Nullam a tortor ex.</p>
+        <div class="flex justify-between">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 50 50"><path d="M38 35h-2V17c0-.6-.4-1-1-1H18v-2h17c1.7 0 3 1.3 3 3v18z" fill="currentColor"/><path d="M37 36.5l-6.8-7.8l1.6-1.4l5.2 6.2l5.2-6.2l1.6 1.4z" fill="currentColor"/><path d="M32 36H15c-1.7 0-3-1.3-3-3V15h2v18c0 .6.4 1 1 1h17v2z" fill="currentColor"/><path d="M18.2 22.7L13 16.5l-5.2 6.2l-1.6-1.4l6.8-7.8l6.8 7.8z" fill="currentColor"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" /></svg>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
