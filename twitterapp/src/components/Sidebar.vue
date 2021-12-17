dc5451a7fb09f10c9a0483988406108b8bb241d3
coutinho-maycon <coutinho.maycon@outlook.com>
2021-12-17 18:48:00 -0300

    Atualização do componente SideBar

---
+++ twitterapp/src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <img class="w-10 h-10 rounded-full" src="https://ef564920920608e03abb-7d34ef097b6ab6c586dfc84157128505.ssl.cf1.rackcdn.com/PostImagem/36734/foto-de-perfil-profissional_o1eh30s23krp31qn41l3havc2fti.JPG" alt="">
+    </div>
+    <div>
+      <p>Maycon_Coutinho</p>
+      <p>@maycon_cout</p>
+    </div>
+    <div>
+      <p>151 <span>Seguindo</span></p>
+      <p>8 <span>Seguidores</span></p>
+    </div>
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
